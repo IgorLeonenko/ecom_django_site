b5dad3423e3b3073fcff51298da41e7fcc4a8235
Igor Leonenko <igrleon@gmail.com>
2024-06-11 18:09:22 +0200

    Add Order model and checkout functionality

--- shop/models.py
+++ shop/models.py
@@ -7,3 +7,14 @@ class Product(models.Model):
     category = models.CharField(max_length=200)
     description = models.TextField()
     image = models.CharField(max_length=500)
+
+
+class Order(models.Model):
+    items = models.CharField(max_length=1000)
+    name = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
+    address = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    state = models.CharField(max_length=255)
+    zip_code = models.CharField(max_length=255)
+    total = models.FloatField()
--- shop/views.py
+++ shop/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.core.paginator import Paginator
-from .models import Product
+from .models import Product, Order
 
 def index(request):
     products = Product.objects.all()
@@ -21,4 +21,24 @@ def index(request):
 def detail(request, id):
     product = Product.objects.get(id=id)
     return render(request, 'shop/detail.html', { "product": product })
+
+def checkout(request):
+    if request.method == "POST":
+        name = request.POST.get('name', '')
+        email = request.POST.get('email', '')
+        address_1 = request.POST.get('address_1', '')
+        address_2 = request.POST.get('address_2', '')
+        address = f"{address_1} {address_2}"
+        city = request.POST.get('city', '')
+        state = request.POST.get('state', '')
+        zip_code = request.POST.get('zip_code', '')
+        items = request.POST.get('items', '')
+        total = request.POST.get('total', '')
+
+        print(f"Total: {items}")
+
+        order = Order(name=name, email=email, address=address, city=city, state=state, zip_code=zip_code, items=items, total=float(total))
+        order.save()
+
+    return render(request, 'shop/checkout.html')
 
